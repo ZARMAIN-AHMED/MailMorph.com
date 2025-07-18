017953de68c17c5ac7ae2bd858c10a51d6c778e2
ZARMAIN-AHMED <nazzshagufta604@gmail.com>
2025-07-18 16:44:58 +0500

    MailMorph.com

--- services/gmail_auth.py
+++ services/gmail_auth.py
@@ -29,7 +29,7 @@ def get_gmail_service():
                 }
             }, SCOPES)
 
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
             with open("token.pkl", "wb") as token:
                 pickle.dump(creds, token)
         except Exception as e:
