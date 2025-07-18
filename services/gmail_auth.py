093fd1dbac5c419bcde2f0d2388f108a0b303d40
ZARMAIN-AHMED <nazzshagufta604@gmail.com>
2025-07-18 16:51:03 +0500

    mailmorph.com

--- services/gmail_auth.py
+++ services/gmail_auth.py
@@ -3,9 +3,6 @@ import pickle
 import streamlit as st
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from dotenv import load_dotenv
-
-load_dotenv()
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
@@ -17,19 +14,8 @@ def get_gmail_service():
 
     if not creds or not creds.valid:
         try:
-            flow = InstalledAppFlow.from_client_config({
-                "installed": {
-                    "client_id": os.getenv("GOOGLE_CLIENT_ID"),
-                    "client_secret": os.getenv("GOOGLE_CLIENT_SECRET"),
-                    "project_id": os.getenv("GOOGLE_PROJECT_ID"),
-                    "auth_uri": os.getenv("GOOGLE_AUTH_URI"),
-                    "token_uri": os.getenv("GOOGLE_TOKEN_URI"),
-                    "auth_provider_x509_cert_url": os.getenv("GOOGLE_AUTH_PROVIDER_CERT_URL"),
-                    "redirect_uris": [os.getenv("GOOGLE_REDIRECT_URI")]
-                }
-            }, SCOPES)
-
-            creds = flow.run_local_server(port=0)
+            flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", SCOPES)
+            creds = flow.run_local_server(port=0)  # Browser-based login
             with open("token.pkl", "wb") as token:
                 pickle.dump(creds, token)
         except Exception as e:
